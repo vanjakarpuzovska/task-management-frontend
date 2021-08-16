dc50618091c4120752c594eb206f77255d664079
vanjakarpuzovska <vanjakarpuzovska@users.noreply.github.com>
2021-08-16 14:33:23 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.64b22f168f9734d202b06fdc74bee0ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de07f552bd5e6bab49fa277a262ab863",
+    "url": "https://vanjakarpuzovska.githup.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "513661d592ed6e9cf336",
+    "url": "https://vanjakarpuzovska.githup.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "060cd8ad47721b1f9f83",
+    "url": "https://vanjakarpuzovska.githup.io/task-management-frontend/static/js/2.6a3a496a.chunk.js"
+  },
+  {
+    "revision": "513661d592ed6e9cf336",
+    "url": "https://vanjakarpuzovska.githup.io/task-management-frontend/static/js/main.919923c7.chunk.js"
+  },
+  {
+    "revision": "eada4dedcb0f714d6bf1",
+    "url": "https://vanjakarpuzovska.githup.io/task-management-frontend/static/js/runtime~main.c183f3b2.js"
+  }
+]);
